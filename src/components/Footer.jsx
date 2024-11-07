b3a78985eefd93e21beba4dc2dc0a4dda2d375df
MakeArtKenji <Kennonirom@gmail.com>
2024-11-07 13:41:55 +0800

    added footer link to YT

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,14 @@ export default function Footer() {
            <footer className="footer footer-center bg-base-100 text-base-content p-4">
             <aside>
                 <p>
-                    Made By&nbsp;
+                    Made By&nbsp;🧿&nbsp;
                     <a 
-                        href="https://example.com" // Replace with your actual URL
+                        href="https://www.youtube.com/@KennOnirom" // Replace with your actual URL
                         className="underline text-primary" 
                         target="_blank" 
                         rel="noopener noreferrer"
                     >
-                       🧿 Kenn Onirom 
+                        Kenn Onirom 
                     </a>
                 </p>
             </aside>
